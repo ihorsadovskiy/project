22b5f3f045c6e04153e72b33ac993dd38a68ea1b
Vitalii Baitaliuk <vitalii@cog1.com>
2018-06-19 13:00:30 +0300

    add server

--- capsure/server.js
+++ capsure/server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
 
-// app.set('views', './build');
-// app.set('view engine', 'html');
 app.use('/build', express.static(__dirname + '/build'));
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
